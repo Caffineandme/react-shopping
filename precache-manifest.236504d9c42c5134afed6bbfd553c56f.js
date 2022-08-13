7a04f34dd68cdfe4b6c70942a6a4f68b01a0508d
mack <mack@macks-MacBook-Pro.local>
2022-08-13 18:00:05 -0500

    Updates

---
+++ precache-manifest.236504d9c42c5134afed6bbfd553c56f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db4e256472f039bcf156",
+    "url": "/react-shopping/static/css/main.aa039b6e.chunk.css"
+  },
+  {
+    "revision": "db4e256472f039bcf156",
+    "url": "/react-shopping/static/js/main.db4e2564.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/react-shopping/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "45d599a840ff1efe516f",
+    "url": "/react-shopping/static/js/2.45d599a8.chunk.js"
+  },
+  {
+    "revision": "3cb7bff1a9568c04f731",
+    "url": "/react-shopping/static/js/runtime~main.3cb7bff1.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-shopping/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1d91636ca4c232eae3d4945582b3970e",
+    "url": "/react-shopping/index.html"
+  }
+];
